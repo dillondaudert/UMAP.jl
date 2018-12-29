c4cb4cd002edb82560ba2fa2d07abbf542982322
Dillon Daudert <dillongdaudert@gmail.com>
2018-12-29 14:27:36 -0500

    Add skipped tests for functions

--- test/umap_tests.jl
+++ test/umap_tests.jl
@@ -1,4 +1,30 @@
 
 @testset "unimplemented tests" begin
-    @test_skip UMAP_([], 0, 0, 0., 0) isa UMAP
+    @testset "constructor" begin
+        @test_skip UMAP_()
+    end
+    
+    @testset "local_fuzzy_simpl_set" begin
+        @test_skip local_fuzzy_simpl_set()
+    end
+    
+    @testset "smooth_knn_dists" begin
+        @test_skip smooth_knn_dists()
+    end
+    
+    @testset "compute_membership_strengths" begin
+        @test_skip compute_membership_strengths()
+    end
+    
+    @testset "simpl_set_embedding" begin
+        @test_skip simpl_set_embedding()
+    end
+    
+    @testset "optimize_embedding" begin
+        @test_skip optimize_embedding()
+    end
+    
+    @testset "spectral_layout" begin
+        @test_skip spectral_layout()
+    end
 end
