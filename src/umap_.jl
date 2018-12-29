d93e76b975efa274598047817bb1765378b1462f
Dillon Daudert <dillongdaudert@gmail.com>
2018-12-29 11:35:02 -0500

    Add more function signatures and docs

--- src/umap_.jl
+++ src/umap_.jl
@@ -4,54 +4,93 @@
 struct UMAP end
 
 """
-    UMAP(X, n_neighbors, n_components, min_dist, epochs) -> embedding
+    UMAP(X, n_neighbors, n_components, min_dist, n_epochs) -> embedding
 
 Embed the data `X` into a `n_components`-dimensional space.
 # Arguments
 - `X`: the dataset to embed
 - `n_neighbors::Integer`: the size of the local neighborhood. Larger values
-capture more global structure in the data, while smaller values capture 
+capture more global structure in the data, while smaller values capture
 more local structure
 - `n_components::Integer`: the dimensionality of the embedding space
-- `min_dist::AbstractFloat`: the minimum spacing of points in the 
-embedding dimension. 
+- `min_dist::AbstractFloat`: the minimum spacing of points in the
+embedding dimension.
 - `n_epochs::Integer`: the number of training epochs for embedding
 optimization
 """
-function UMAP(X, 
+function UMAP(X,
               n_neighbors::Integer,
               n_components::Integer,
               min_dist::AbstractFloat,
-              epochs::Integer) 
+              n_epochs::Integer)
     # argument checking
-    
+
     # main algorithm
-    fuzzy_simpl_set = local_fuzzy_simpl_set(X, n_neighbors)
-    topological_repr = fuzzy_union(fuzzy_simpl_set)
+    if length(X) < 4096:
+        # compute all pairwise distances
+    else:
+        # approximate knn distances
+
+    fs_set_graph = local_fuzzy_simpl_set(X, n_neighbors)
     # initialize low-d embedding with spectral embedding
     X_embed = spectral_embed(topological_repr, n_components)
-    # refine embedding with SGD 
+    # refine embedding with SGD
     X_embed = optim_embed(topological_repr, X_embed, min_dist, n_epochs)
+
+    # TODO: if target variable y is passed, then construct target graph
+    #       in the same manner and do a fuzzy simpl set intersection
     return
 end
 
 """
-    local_fuzzy_simpl_set(X, n_neighbors) -> knn, knn_dists
+    local_fuzzy_simpl_set(X, n_neighbors) -> fuzzy_simpl_set::SparseMatrixCSC
 
-Construct the local fuzzy simplicial sets of each point in `X` by 
+Construct the local fuzzy simplicial sets of each point in `X` by
 finding the approximate nearest `n_neighbors`, normalizing the distances
 on the manifolds, and converting the metric space to a simplicial set.
 """
-function local_fuzzy_simpl_set(X, n_neighbors) 
-    knn, knn_dists = nndescent(X, n_neighbors)
-    sigma = smooth_knn_dists(knn_dists, n_neighbors)
-    fuzzy_simpl_set_0 = X
-    fuzzy_simpl_set_1 = # {([x, y], 0) for all x in X, y in X}
-    # cont ...
+function local_fuzzy_simpl_set(X, n_neighbors)
+    knns, dists = nndescent(X, n_neighbors)
+    σ, ρ = smooth_knn_dists(dists, n_neighbors)
+
+    rows, cols, vals = compute_membership_strengths(knns, dists, σ, ρ)
+    fs_set = sparse(rows, cols, vals) # sparse matrix M[i, j] = vᵢⱼ where
+                                      # vᵢⱼ is the probability that j is in the
+                                      # simplicial set of i
+    return dropzeros(fs_set + fs_set' - fs_set .* fs_set')
 end
 
-function smooth_knn_dists(knn_dists, n_neighbors) end
+"""
+    smooth_knn_dists(dists, k; <kwargs>) -> knn_dists, nn_dists
 
-function fuzzy_union(fuzzy_simpl_set) end
-function spectral_embed(topological_repr, n_components) end
-function optim_embed(topological_repr, X_embed, min_dist, n_epochs) end
+Compute the distances to the nearest neighbors for a continuous value `k`. Returns
+the approximated distances to the kth nearest neighbor (`knn_dists`)
+and the nearest neighbor (nn_dists) from each point.
+
+# Arguments
+...
+"""
+function smooth_knn_dists(knn_dists, k::AbstractFloat; n_iter::Integer=64) end
+
+"""
+    compute_membership_strengths(knns, dists, σ, ρ) -> rows, cols, strengths
+
+Compute the membership strengths for the 1-skeleton of each fuzzy simplicial set.
+"""
+function compute_membership_strengths(knns, dists, σ, ρ) end
+
+"""
+    simpl_set_embedding(X, fs_set_graph, n_components, n_epochs; <kwargs>) -> embedding
+
+Create an embedding by minimizing the fuzzy set cross entropy between the
+fuzzy simplicial set 1-skeletons of the data in high and low dimensional
+spaces.
+"""
+function simpl_set_embedding(X, fs_set_graph::SparseMatrixCSC, n_components, n_epochs;
+                             init::Symbol=:spectral) end
+
+"""
+Optimize an embedding by minimizing the fuzzy set cross entropy between the high and low
+dimensional simplicial sets using stochastic gradient descent.
+"""
+function optimize_embedding(head_embedding, tail_embedding, a...) end
