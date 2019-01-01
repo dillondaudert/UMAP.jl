521488f64ff65cefaec96c3afbc93f68de8c8b0e
Dillon Daudert <dillongdaudert@gmail.com>
2019-01-01 13:15:12 -0500

    Add smooth_knn_dists tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test
 using SparseArrays
 using LinearAlgebra
-using UMAP: compute_membership_strengths, spectral_layout, optimize_embedding
+using UMAP: compute_membership_strengths, smooth_knn_dists, smooth_knn_dist, spectral_layout, optimize_embedding
 
 include("umap_tests.jl")
--- test/umap_tests.jl
+++ test/umap_tests.jl
@@ -12,7 +12,22 @@
     end
     
     @testset "smooth_knn_dists" begin
-        @test_skip smooth_knn_dists()
+        dists = [0., 1., 2., 3., 4., 5.]
+        rho = 1
+        sigma = smooth_knn_dist(dists, 6, 100, rho, 1e-5)
+        psum(ds, r, s) = sum(exp.(-max.(ds .- r, 0.) ./ s))
+        @test psum(dists, rho, sigma) - log2(6) < 1e-5
+        
+        knn_dists = [0. 0. 0.;
+                     1. 2. 3.;
+                     2. 4. 5.;
+                     3. 4. 5.;
+                     4. 6. 6.;
+                     5. 6. 10.]
+        rhos, sigmas = smooth_knn_dists(knn_dists,
+                                        6)
+        @test rhos == [1., 2., 3.]
+        @test all([psum(knn_dists[:,i], rhos[i], sigmas[i]) for i in 1:3] .- log2(6) .< 1e-5)
     end
     
     @testset "compute_membership_strengths" begin
