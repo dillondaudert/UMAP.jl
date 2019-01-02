7f97ed6567fb7aa64e729ab3002b05d701a41134
Dillon Daudert <dillongdaudert@gmail.com>
2019-01-02 18:44:09 -0500

    Fix newlines in docs

--- src/umap_.jl
+++ src/umap_.jl
@@ -22,26 +22,15 @@ global structure in the data, while small values capture more local structure.
 - `metric::SemiMetric = Euclidean()`: the metric to calculate distance in the input space
 - `n_epochs::Integer = 300`: the number of training epochs for embedding optimization
 - `learning_rate::AbstractFloat = 1.`: the initial learning rate during optimization
-- `init::Symbol = :spectral`: how to initialize the output embedding; valid options 
-are `:spectral` and `:random`
+- `init::Symbol = :spectral`: how to initialize the output embedding; valid options are `:spectral` and `:random`
 - `min_dist::AbstractFloat = 0.1`: the minimum spacing of points in the output embedding
-- `spread::AbstractFloat = 1.0`: the effective scale of embedded points. Determines how
-clustered embedded points are in combination with `min_dist`.
-- `set_operation_ratio::AbstractFloat = 1.0`: interpolates between fuzzy set union and 
-fuzzy set intersection when constructing the UMAP graph (global fuzzy simplicial set).
-The value of this parameter should be between 1.0 and 0.0: 1.0 indicates pure fuzzy union,
-while 0.0 indicates pure fuzzy intersection.
-- `local_connectivity::Integer = 1`: the number of nearest neighbors that should be assumed
-to be locally connected. The higher this value, the more connected the manifold becomes. This
-should not be set higher than the intrinsic dimension of the manifold.
-- `repulsion_strength::AbstractFloat = 1.0`: the weighting of negative samples during the
-optimization process. 
-- `neg_sample_rate::Integer = 5`: the number of negative samples to select for each positive 
-sample. Higher values will increase computational cost but result in slightly more accuracy.
-- `a::AbstractFloat = nothing`: this controls the embedding. By default, this is determined
-automatically by `min_dist` and `spread`.
-- `b::AbstractFloat = nothing`: this controls the embedding. By default, this is determined
-automatically by `min_dist` and `spread`.
+- `spread::AbstractFloat = 1.0`: the effective scale of embedded points. Determines how clustered embedded points are in combination with `min_dist`.
+- `set_operation_ratio::AbstractFloat = 1.0`: interpolates between fuzzy set union and fuzzy set intersection when constructing the UMAP graph (global fuzzy simplicial set). The value of this parameter should be between 1.0 and 0.0: 1.0 indicates pure fuzzy union, while 0.0 indicates pure fuzzy intersection.
+- `local_connectivity::Integer = 1`: the number of nearest neighbors that should be assumed to be locally connected. The higher this value, the more connected the manifold becomes. This should not be set higher than the intrinsic dimension of the manifold.
+- `repulsion_strength::AbstractFloat = 1.0`: the weighting of negative samples during the optimization process. 
+- `neg_sample_rate::Integer = 5`: the number of negative samples to select for each positive sample. Higher values will increase computational cost but result in slightly more accuracy.
+- `a::AbstractFloat = nothing`: this controls the embedding. By default, this is determined automatically by `min_dist` and `spread`.
+- `b::AbstractFloat = nothing`: this controls the embedding. By default, this is determined automatically by `min_dist` and `spread`.
 """
 function UMAP_(X::Vector{V},
                n_neighbors::Integer = 15,
