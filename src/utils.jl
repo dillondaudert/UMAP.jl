2c5635eca4a130eccadb90bd16cb21b2dd7d2517
Dillon Daudert <dillongdaudert@gmail.com>
2019-01-06 17:27:57 -0500

    pairwise_knn accepts abstract matrix as input

--- src/utils.jl
+++ src/utils.jl
@@ -1,11 +1,12 @@
 
-function pairwise_knn(X::AbstractMatrix, n_neighbors, metric) where {V <: AbstractVector}
-    pairwise_dists = [Array{eltype(V)}(undef, length(X)) for _ in 1:length(X)]
-    @inbounds for i in 1:length(X), j in 1:length(X)
-        pairwise_dists[i][j] = evaluate(metric, X[i], X[j])
-    end
-    # get indices of closest neighbors (array of array of indices)
-    knns = [p[2:n_neighbors+1] for p in sortperm.(pairwise_dists)]
-    dists = [pairwise_dists[i][knns[i]] for i in 1:length(knns)]
-    return hcat(knns...), hcat(dists...)    
+function pairwise_knn(X::AbstractMatrix{S}, 
+                      n_neighbors, 
+                      metric) where {S <: AbstractFloat, V <: AbstractVector}
+    num_points = size(X, 2)
+    all_dists = Array{S}(undef, num_points, num_points)
+    pairwise!(all_dists, metric, X)
+    # all_dists is symmetric distance matrix
+    knns = [partialsortperm(view(all_dists, :, i), 2:n_neighbors+1) for i in 1:num_points]
+    dists = [all_dists[:, i][knns[i]] for i in eachindex(knns)]
+    return hcat(knns...), hcat(dists...)
 end
