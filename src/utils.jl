d1e08289762fbc97dc14bad2afda20c51328c029
Dillon Daudert <dillongdaudert@gmail.com>
2019-01-13 17:04:38 -0500

    Add type asserts to pairwise_knn; add fuzzy_set_op functions for clarity and type stability

--- src/utils.jl
+++ src/utils.jl
@@ -3,21 +3,31 @@
 # return the nearest k to each point v, other than v itself
 function pairwise_knn(X::AbstractMatrix{S}, 
                       n_neighbors, 
-                      metric) where {S <: AbstractFloat, V <: AbstractVector}
+                      metric) where {S <: AbstractFloat}
     num_points = size(X, 2)
     all_dists = Array{S}(undef, num_points, num_points)
     pairwise!(all_dists, metric, X)
     # all_dists is symmetric distance matrix
-    knns = [partialsortperm(view(all_dists, :, i), 2:n_neighbors+1) for i in 1:num_points]
-    dists = [all_dists[:, i][knns[i]] for i in eachindex(knns)]
-    return hcat(knns...), hcat(dists...)
+    knns_ = [partialsortperm(view(all_dists, :, i), 2:n_neighbors+1) for i in 1:num_points]
+    dists_ = [all_dists[:, i][knns_[i]] for i in eachindex(knns_)]
+    knns = hcat(knns_...)::Matrix{Int}
+    dists = hcat(dists_...)::Matrix{S}
+    return knns, dists
 end
 
 
 # combine local fuzzy simplicial sets 
 function combine_fuzzy_sets(fs_set::AbstractMatrix{T}, 
                             set_op_ratio::T) where {T <: AbstractFloat}
-    res = set_op_ratio .* (fs_set .+ fs_set' .- fs_set .* fs_set') .+ (one(T) - set_op_ratio) .* (fs_set .* fs_set')
+    return set_op_ratio .* fuzzy_set_union(fs_set) .+ 
+           (one(T) - set_op_ratio) .* fuzzy_set_intersection(fs_set)
     
-    return res
+end
+
+function fuzzy_set_union(fs_set::AbstractMatrix)
+    return fs_set .+ fs_set' .- (fs_set .* fs_set')
+end
+
+function fuzzy_set_intersection(fs_set::AbstractMatrix)
+    return fs_set .* fs_set'
 end
