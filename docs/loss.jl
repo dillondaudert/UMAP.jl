260603608ff0be6178363d0b093c6affa1a85618
Dillon Daudert <dillongdaudert@gmail.com>
2019-10-30 00:02:26 -0400

    Update docs

--- docs/loss.jl
+++ docs/loss.jl
@@ -1,12 +1,12 @@
 # notes on the fuzzy set cross entropy loss for UMAP
 
 """
-Given two fuzzy simplicial sets, we can consider the 1-skeleta as 
+Given two fuzzy simplicial sets, we can consider the 1-skeleta as
 a fuzzy graph (i.e. a set of edges, where each edge has a probability of existing
-in the graph). The two sets (of edges) can be compared by computing the 
+in the graph). The two sets (of edges) can be compared by computing the
 set cross entropy.
 
-For a set A and membership functions μ: A → [0, 1], ν: A → [0, 1], the set cross 
+For a set A and membership functions μ: A → [0, 1], ν: A → [0, 1], the set cross
 entropy is:
 """
 
@@ -19,7 +19,7 @@ function cross_entropy(A::Set, μ, ν)
 end
 
 """
-The loss function can be generalized to 𝒍-skeleta by the weighted sum of the 
+The loss function can be generalized to 𝒍-skeleta by the weighted sum of the
 set cross entropies of the fuzzy sets of 𝑖-simplices. That is,
 
 C_l(X, Y) = Σ_{i=1}^{l}(λᵢ * C(Xᵢ, Yᵢ)),
@@ -42,7 +42,7 @@ C(A, μ, ν) = - Σ_{a ∈ A} ( μ(a) * log(ν(a)) + (1 - μ(a)) * log(1 - ν(a)
 function cross_entropy(A::Set, μ, ν)
     loss = 0
     for a ∈ A
-        loss += μ(a) * log(ν(a)) + (1 - μ(a)) * log(1 - ν(a)) 
+        loss += μ(a) * log(ν(a)) + (1 - μ(a)) * log(1 - ν(a))
     end
     return -loss
 end
@@ -51,7 +51,7 @@ end
 Instead of calculating the loss over the entire set (if our set is comprised
 of the 1-simplices, then calculating this loss would have time complexity
 𝒪(n²)), we can sample elements with probability μ(a) and update according to
-the value ν(a). This takes care of the μ(a) * log(ν(a)) term. For the 
+the value ν(a). This takes care of the μ(a) * log(ν(a)) term. For the
 negative samples, elements are sampled uniformly and assumed to have μ(a) = 0.
 This results in a sampling distribution of
 
@@ -73,7 +73,7 @@ and constants `α`, `β`:
 The approximation parameters `α`, `β` are chosen by non-linear least squares
 fitting of the following function ψ:
 """
-ψ(x, y, σ, min_dist) = 1 if σ(x, y) ≤ min_dist else exp(-(σ(x, y) - min_dist)) 
+ψ(x, y, σ, min_dist) = 1 if σ(x, y) ≤ min_dist else exp(-(σ(x, y) - min_dist))
 
 """
 Optimizing the embedding is therefore accomplished by the following, where
@@ -83,12 +83,25 @@ a differentiable distance measure between points in `Y_emb`, and `ϕ` is the
 differentiable approximation to the fuzzy set membership function for the
 simplices in the target embedding.
 """
-function optimize_embedding(fs_set, Y_emb, σ, ϕ, n_epochs)
+function optimize_embedding(fs_set, Y_emb, σ, ϕ, n_epochs, n_neg_samples)
     η = 1
-    ∇logϕ(x, y) = gradient((_x, _y) -> (log ∘ ϕ)(_x, _y, σ), x, y)
+    ∇logϕ(x, y) = gradient((_x, _y) -> log(ϕ(_x, _y, σ)), x, y)
+    ∇log1_ϕ(x, y) = gradient((_x, _y) -> log(1 - ϕ(_x, _y, α)), x, y)
     for e in 1:n_epochs
         for (a, b, p) in fs_set₁ # 1-simplices here
             if rand() ≤ p
                 ∂a, ∂b = η * ∇logϕ(Y_emb[a], Y_emb[b])
-                Y_emb[a] += ∂a
-                # Y_emb[b] += ∂b
+                Y_emb[a] -= ∂a
+                # Y_emb[b] -= ∂b
+                for _ in 1:n_neg_samples
+                    c = sample(Y_emb)
+                    ∂a, ∂c = η * ∇log1_ϕ(Y_emb[a], Y_emb[c])
+                    Y_emb[a] -= ∂a
+                    # Y_emb[c] -= ∂c
+                end
+            end
+        end
+        η = 1 - e/n_epochs
+    end
+    return Y_emb
+end
