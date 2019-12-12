8352801f6393d7956c979eaebd179ba9f622603c
Dillon Daudert <dillongdaudert@gmail.com>
2019-12-12 17:30:07 -0500

    Layouts messing around - refactor refunc

--- src/layouts.jl
+++ src/layouts.jl
@@ -20,26 +20,26 @@ function optimize_embedding!(graph,
                              spread,
                              gamma,
                              neg_sample_rate,
-                             _a=nothing,
-                             _b=nothing)
-    a, b = fit_ab(min_dist, spread, _a, _b)
+                             a,
+                             b)
 
     alpha = initial_alpha
     for e in 1:n_epochs
-        embedding = _optimize_embedding!(graph, embedding, alpha, min_dist, spread, gamma, neg_sample_rate, a, b)
+        embedding = _optimize_embedding!(graph, embedding, alpha, gamma, neg_sample_rate, a, b)
         alpha = initial_alpha*(1 - e//n_epochs)
     end
 
     return embedding
 end
 
-@views function _optimize_embedding!(graph, embedding, alpha, min_dist, spread, gamma, neg_sample_rate, a, b)
+function _optimize_embedding!(graph, embedding::Matrix{T}, alpha::T, gamma::T, neg_sample_rate::Integer, a::T, b::T) where T <: Real
     @inbounds for i in 1:size(graph, 2)
         for ind in nzrange(graph, i)
             j = rowvals(graph)[ind]
             p = nonzeros(graph)[ind]
             if rand() <= p
-                delta = pos_grad_coef(SqEuclidean(), embedding[:, i], embedding[:, j], a, b)
+                pdist = evaluate(SqEuclidean(), view(embedding, :, i), view(embedding, :, j))
+                delta = pos_grad_coef(pdist, a, b)
                 @simd for d in 1:size(embedding, 1)
                     grad = clamp(delta * (embedding[d,i] - embedding[d,j]), -4, 4)
                     embedding[d,i] += alpha * grad
@@ -49,7 +49,8 @@ end
                 for _ in 1:neg_sample_rate
                     k = rand(1:size(graph, 2))
                     i != k || continue # don't evaluate if the same point
-                    delta = neg_grad_coef(SqEuclidean(), embedding[:, i], embedding[:, k])
+                    ndist = evaluate(SqEuclidean(), view(embedding, :, i), view(embedding, :, k))
+                    delta = neg_grad_coef(ndist, gamma, a, b)
                     @simd for d in 1:size(embedding, 1)
                         if delta > 0
                             grad = clamp(delta * (embedding[d, i] - embedding[d, k]), -4, 4)
@@ -66,14 +67,12 @@ end
     return embedding
 end
 
-function pos_grad_coef(metric, head::V, tail::V, a, b) where {V <: AbstractVector}
-    dist = evaluate(metric, head, tail)
-    delta = (-2 * a * b * dist^(b-1))/(1 + a*dist^b)
-    return max(zero(eltype(head)), delta)
+function pos_grad_coef(dist::T, a, b) where T
+    delta = (-T(2) * a * b * dist^(b-one(T)))/(one(T) + a*dist^b)
+    return max(zero(T), delta)
 end
 
-function neg_grad_coef(metric, head::V, tail::V, gamma, a, b) where {V <: AbstractVector}
-    dist = evaluate(metric, head, tail)
-    delta = (2 * gamma * b) / ((dist + 1//1000)*(1 + a * dist^b))
-    return max(zero(eltype(head)), delta)
+function neg_grad_coef(dist::T, gamma, a, b) where T
+    delta = (T(2) * gamma * b) / ((dist + sqrt(eps(T)))*(one(T) + a * dist^b))::T
+    return max(zero(T), delta)
 end
