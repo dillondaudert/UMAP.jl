285ce2a86df2d6335cdafc1afa9af68b9ee77f07
Dillon Daudert <dillongdaudert@gmail.com>
2020-05-29 16:30:53 -0400

    Update new knn_search draft to use named tuples

--- src/neighbors.jl
+++ src/neighbors.jl
@@ -3,10 +3,9 @@
 abstract type NeighborParams end
 
 # for finding approximate nearest neighbors
-struct DescentNeighbors{M, V, K} <: NeighborParams
+struct DescentNeighbors{M, K} <: NeighborParams
     n_neighbors::Int
     metric::M
-    cols::V
     kwargs::K
 end
 
@@ -16,45 +15,38 @@ struct PrecomputedNeighbors{M} <: NeighborParams
     dists::M
 end
 
-# utility function for getting particular views of the data
-function get_data_view(data, knn_params::DescentNeighbors{M, ::Nothing}) where M
-    # there is only one view: the entire dataset
-    return data
-end
-
-function get_data_view(data, knn_params::DescentNeighbors)
-    return Tables.getcolumn(data, knn_params.cols)
-end
-
 # finding neighbors
 
-# broadcast over the knn parameters
-function knn_search(data, knn_params)
-    return knn_search.((data,), knn_params)
+# for each view, find the knns (fit)
+function knn_search(data::NamedTuple{T}, knn_params::NamedTuple{T}) where T
+    view_graphs = [knn_search(data[name], knn_params[name]) for name in keys(data)]
+    return NamedTuple{T}(tuple(view_graphs...))
 end
 
 # NOTE: here, data will need to be the UMAP result struct
-function knn_search(result, queries, knn_params)
+# for each view, find the knn graph for the query data
+function knn_search(result, queries::NamedTuple{T}, knn_params::NamedTuple{T}) where T
     # if result::UMAPResult{DS, DT, C, V} where result.views::V, then
-    # V <: UMAPViewResult (in other cases, it will be a vector of view results)
-    return knn_search.((result.data,), (queries,), knn_params, result.views)
+    # V will be a named tuple of UMAPViewResult here
+    view_graphs = [knn_search(result.data[name],
+                              queries[name],
+                              knn_params[name],
+                              result.views[name]) for name in keys(result.data)]
+    return NamedTuple{T}(tuple(view_graphs...))
 end
 
 # find approximate neighbors
 
 function knn_search(data, knn_params::DescentNeighbors)
-    data_view = get_data_view(data, knn_params)
-    knn_graph = nndescent(data_view, knn_params.n_neighbors, knn_params.metric; knn_params.kwargs...)
+    knn_graph = nndescent(data, knn_params.n_neighbors, knn_params.metric; knn_params.kwargs...)
     return knn_matrices(knn_graph)
 end
 
-function knn_search(data, queries, knn_params::DescentNeighbors, view_result) where M
-    data_view = get_data_view(data, knn_params)
+function knn_search(data, queries, knn_params::DescentNeighbors, view_result)
     orig_knn_graph = HeapKNNGraph(data_view,
                                   knn_params.metric,
                                   result_view.knns,
                                   result_view.dists)
-    queries_view = get_data_view(queries, knn_params)
     return search(orig_knn_graph, queries_view, knn_params.n_neighbors; knn_params.kwargs...)
 end
 
