fc34bdfa3d7bae011fda3f027ae5e3762c43d5b8
ericphanson <5846501+ericphanson@users.noreply.github.com>
2020-12-10 16:22:35 +0000

    fix bugs

--- src/umap_.jl
+++ src/umap_.jl
@@ -85,10 +85,10 @@ function Embedding(g::Graph; kwargs...)
 end
 
 function Embedding(g1::Graph, g2::Graph; mix_weight=0.5, kwargs...)
-    return Embedding(fuzzy_intersection(g1, g2); mix_weight=mix_weight, kwargs...)
+    return Embedding(fuzzy_intersection(g1, g2; mix_weight=mix_weight); kwargs...)
 end
 
-Embedding(args...; kwargs...) = Embedding(map(Graph, args); kwargs...)
+Embedding(args...; kwargs...) = Embedding(map(Graph, args)...; kwargs...)
 
 
 const SMOOTH_K_TOLERANCE = 1e-5
