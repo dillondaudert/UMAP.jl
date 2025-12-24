1e25e2c038a065b2d471cd0a04ac5472f7f0c3e5
Dillon Daudert <dillongdaudert@gmail.com>
2025-12-24 11:22:10 -0500

    Move opt params to optimize.jl

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,4 +1,31 @@
 
+
+"""
+    OptimizationParams(n_epochs, learning_rate, repulsion_strength, neg_sample_rate)
+
+Parameters for controlling the optimization process.
+"""
+struct OptimizationParams
+    "The number of epochs to perform optimization"
+    n_epochs::Int
+    "The initial learning rate for optimization (decreases each epoch)"
+    lr::Float64
+    "The weighting of negative samples during optimization"
+    repulsion_strength::Float64
+    """
+    The number of negative samples to select for each positive sample.
+    Higher values increase computational cost but result in slightly better accuracy.
+    """
+    neg_sample_rate::Int
+    function OptimizationParams(n_epochs, lr, repulsion_strength, neg_sample_rate)
+        n_epochs > 0 || throw(ArgumentError("n_epochs must be greater than 0, got $n_epochs"))
+        lr ≥ 0 || throw(ArgumentError("learning_rate must be non-negative, got $lr"))
+        neg_sample_rate ≥ 0 || throw(ArgumentError("neg_sample_rate must be non-negative, got $neg_sample_rate"))
+        return new(n_epochs, lr, repulsion_strength, neg_sample_rate)
+    end
+end
+
+
 function optimize_embedding!(embedding, umap_graph, tgt_params, opt_params)
     _opt_params = opt_params
     for e in 1:opt_params.n_epochs
