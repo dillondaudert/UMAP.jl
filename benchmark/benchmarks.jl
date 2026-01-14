f2efc8d8ff819cdcf04f979eb19e38be576b206b
Dillon Daudert <dillongdaudert@gmail.com>
2026-01-14 10:36:22 -0500

    Add PkgBenchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -18,10 +18,10 @@ include("simplicial_sets_bench.jl")
 include("embeddings_bench.jl")
 include("optimize_bench.jl")
 
-suite = BenchmarkGroup()
+SUITE = BenchmarkGroup()
 
 # Integration benchmarks for UMAP.fit
-suite["fit"] = BenchmarkGroup(["integration"])
+SUITE["fit"] = BenchmarkGroup(["integration"])
 
 const N_POINTS = [1000, 10_000]
 const IN_DIMS = [10, 50]
@@ -29,7 +29,7 @@ const OUT_DIMS = [2, 10]
 
 for (n_points, in_dims, out_dims) in Iterators.product(N_POINTS, IN_DIMS, OUT_DIMS)
     data = matrix_data(n_points, in_dims)
-    suite["fit"]["matrix"]["$(n_points)x$(in_dims)x$(out_dims)"] = @benchmarkable(
+    SUITE["fit"]["matrix"]["$(n_points)x$(in_dims)x$(out_dims)"] = @benchmarkable(
         UMAP.fit($data, $out_dims; n_neighbors=15, n_epochs=100),
         setup=(GC.gc()),
         evals=1,
@@ -37,7 +37,7 @@ for (n_points, in_dims, out_dims) in Iterators.product(N_POINTS, IN_DIMS, OUT_DI
     )
 
     vec_data = vecvec_data(n_points, in_dims)
-    suite["fit"]["vectors"]["$(n_points)x$(in_dims)x$(out_dims)"] = @benchmarkable(
+    SUITE["fit"]["vectors"]["$(n_points)x$(in_dims)x$(out_dims)"] = @benchmarkable(
         UMAP.fit($vec_data, $out_dims; n_neighbors=15, n_epochs=100),
         setup=(GC.gc()),
         evals=1,
@@ -46,13 +46,13 @@ for (n_points, in_dims, out_dims) in Iterators.product(N_POINTS, IN_DIMS, OUT_DI
 end
 
 # Add component benchmark suites
-suite["simplicial"] = SimplicialBench.simplicial_suite
-suite["embeddings"] = EmbeddingsBench.embeddings_suite
-suite["optimize"] = OptimizeBench.optimize_suite
+SUITE["simplicial"] = SimplicialBench.simplicial_suite
+SUITE["embeddings"] = EmbeddingsBench.embeddings_suite
+SUITE["optimize"] = OptimizeBench.optimize_suite
 
 # Only tune and run when executed directly
 if abspath(PROGRAM_FILE) == @__FILE__
-    tune!(suite)
-    results = run(suite, verbose=true, seconds=1)
+    tune!(SUITE)
+    results = run(SUITE, verbose=true, seconds=1)
     print(results)
 end
